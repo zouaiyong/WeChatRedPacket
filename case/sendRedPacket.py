83c2ce59bb6c17337c4da30fe4bd69f9a6552286
zouay <492346157@qq.com>
2019-01-12 23:48:58 +0800

    修改测试

--- case/sendRedPacket.py
+++ case/sendRedPacket.py
@@ -40,7 +40,8 @@ class sendRedPacket(mutil.TestCaseUtil.TestCaseUtil):
                 if  user.get('type')==1:
                     try:
                         self.driver.launch_app()
-                        sleep(5)
+                        self.driver.wait_activity('com.tencent.mm.ui.LauncherUI',10)
+                        #sleep(5)
                         id=self.driver.find_element_by_android_uiautomator('new UiSelector().description("Search")')
                         #id=self.driver.find_element_by_android_uiautomator('new UiSelector().resourceId("com.tencent.mm:id/ij")')
                         id.click()
